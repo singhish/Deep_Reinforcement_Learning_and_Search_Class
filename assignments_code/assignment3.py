bb4f96055710fa0f8dc3bf93628c78c959cfcc8a
ishr <ishratsingh00@gmail.com>
2020-11-06 16:29:31 -0500

    add assignment 2 and 3

--- assignments_code/assignment3.py
+++ assignments_code/assignment3.py
@@ -4,29 +4,50 @@ from torch import nn
 from environments.environment_abstract import Environment, State
 import numpy as np
 
+import random; random.seed(0)
+
 
 def relu_forward(inputs):
-
-    return outputs
+    inputs[inputs < 0] = 0
+    return inputs
 
 
 def relu_backward(grad, inputs):
-
-    return inputs_grad
+    return grad
 
 
 def linear_forward(inputs, weights, biases):
-
-    return outputs
+    return (inputs @ weights.T) + biases
 
 
 def linear_backward(grad, inputs, weights, biases):
-
+    weights_grad = grad.T @ inputs
+    biases_grad = np.sum(grad, axis=0)
+    inputs_grad = grad @ weights
     return weights_grad, biases_grad, inputs_grad
 
 
 def get_dqn():
 
+    class DQN(nn.Module):
+
+        def __init__(self):
+            super(DQN, self).__init__()
+            self.layers = nn.Sequential(
+                nn.Linear(100, 75),
+                nn.ReLU(),
+                nn.Linear(75, 50),
+                nn.ReLU(),
+                nn.Linear(50, 4)
+            )
+
+        def forward(self, x):
+            x = x.float()
+            x = self.layers(x)
+            return x
+
+
+    dqn = DQN()
     return dqn
 
 
@@ -34,10 +55,17 @@ def deep_q_learning_step(env: Environment, state: State, dqn: nn.Module, dqn_tar
                          discount: float, batch_size: int, optimizer, device, replay_buffer: List):
     # get action
     dqn.eval()
+    if random.random() < epsilon:
+        action = random.choice(env.get_actions())
+    else:
+        state_nnet = torch.tensor(env.state_to_nnet_input(state), device=device)
+        action = torch.argmax(dqn(state_nnet)).item()
 
     # get transition
+    state_next, reward = env.sample_transition(state, action)
 
     # add to replay buffer
+    replay_buffer.append((state, action, reward, state_next))
 
     # sample from replay buffer and train
     batch_idxs = np.random.randint(len(replay_buffer), size=batch_size)
@@ -61,10 +89,17 @@ def deep_q_learning_step(env: Environment, state: State, dqn: nn.Module, dqn_tar
     optimizer.zero_grad()
 
     # compute target
+    target = torch.unsqueeze(
+        torch.tensor(
+            [(r if is_ter else r + discount * torch.max(dqn_target(s_p)))
+             for r, s_p, is_ter
+             in zip(rewards, states_next_nnet, is_terminal)]), 1).double()
 
     # get output of dqn
+    output = dqn(states_nnet).gather(1, actions).double()
 
     # loss
+    loss = nn.MSELoss()(output, target)
 
     # backpropagation
     loss.backward()
